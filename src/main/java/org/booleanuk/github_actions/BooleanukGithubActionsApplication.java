8122794fc66583d542147a8e8d5834b9efb54ebf
hannaklh <hannaklh@gmail.com>
2025-09-04 15:00:11 +0200

    init

---
+++ src/main/java/org/booleanuk/github_actions/BooleanukGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package org.booleanuk.github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanukGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooleanukGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/booleanuk/github_actions/BooleanukGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.booleanuk.github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooleanukGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
